6fee1ad85a6cca6ddd3411decec8660ace642ee5
jlhb1984 <jlhb1984@gmail.com>
2024-09-29 13:39:20 -0500

    Se agregó el menú con while.

--- data_analyzer01.py
+++ data_analyzer01.py
@@ -1,59 +1,63 @@
 import pandas as pd
 import numpy as np
 
-number_plate=input("Digite la placa del vehículo en mayúscula: ")
-print(number_plate)
+print("data_analyzer01.")
+option=input("1. Digitar placa del vehículo.\n2. Salir.")
 
-#Creación de la ruta:
-path='Historical_locations_'+number_plate+'.csv'
-print(path)
+while option!=2:
+    number_plate=input("Digite la placa del vehículo en mayúscula: ")
+    print(number_plate)
 
-#Carga del dataframe del vehículo.
-df_book=pd.read_csv(path)
-df_book.info()
+    #Creación de la ruta:
+    path='Historical_locations_'+number_plate+'.csv'
+    print(path)
 
-#Selección de los atributos mas significativos.
-df_book_filter01=df_book[['Unit','Events','Date','Landmark']]
+    #Carga del dataframe del vehículo.
+    df_book=pd.read_csv(path)
+    df_book.info()
 
-#Selección de los eventos mas significativos.
-df_book_land_pu=df_book_filter01[
-    (df_book['Events']=='Geofence IN')|
-    (df_book['Events']=='Geofence OUT')|
-    (df_book['Events']=='Landmark IN') |
-    (df_book['Events']=='Landmark OUT')|
-    (df_book['Events']=='POWER UP')|
-    (df_book['Events']=='Ignition ON')]
-#print(df_book_land_pu)
+    #Selección de los atributos mas significativos.
+    df_book_filter01=df_book[['Unit','Events','Date','Landmark']]
 
-#Aquí irá los valores del atributo estudiado del dataframe del vehículo.
-landmarks=df_book_land_pu.loc[:,'Landmark']
-print("Landmarks info: ")
-#print(landmarks)
+    #Selección de los eventos mas significativos.
+    df_book_land_pu=df_book_filter01[
+        (df_book['Events']=='Geofence IN')|
+        (df_book['Events']=='Geofence OUT')|
+        (df_book['Events']=='Landmark IN') |
+        (df_book['Events']=='Landmark OUT')|
+        (df_book['Events']=='POWER UP')|
+        (df_book['Events']=='Ignition ON')]
+    #print(df_book_land_pu)
 
-#Elimino los registros duplicados para el atributo estudiado.
-landmarks.drop_duplicates(inplace=True)
-#print(landmarks)
-#print("Total de registros: ",landmarks.count())
+    #Aquí irá los valores del atributo estudiado del dataframe del vehículo.
+    landmarks=df_book_land_pu.loc[:,'Landmark']
+    print("Landmarks info: ")
+    #print(landmarks)
 
-#Creo la lista de geofences:
-df_book_geofences=pd.read_csv('/home/jose/git/tso/Colab/Geofences InOut Report.csv')
-df_book_geofences_GeofenceName=df_book_geofences['GeofenceName']
-df_book_geofences_GeofenceName.drop_duplicates(inplace=True)
-#print(df_book_geofences_GeofenceName.head(2))
-geofences=df_book_geofences_GeofenceName
-total_geofences=geofences.count()
-print("Total geofences: ",total_geofences)
+    #Elimino los registros duplicados para el atributo estudiado.
+    landmarks.drop_duplicates(inplace=True)
+    #print(landmarks)
+    #print("Total de registros: ",landmarks.count())
 
-print("Wrong logs: ")
+    #Creo la lista de geofences:
+    df_book_geofences=pd.read_csv('/home/jose/git/tso/Colab/Geofences InOut Report.csv')
+    df_book_geofences_GeofenceName=df_book_geofences['GeofenceName']
+    df_book_geofences_GeofenceName.drop_duplicates(inplace=True)
+    #print(df_book_geofences_GeofenceName.head(2))
+    geofences=df_book_geofences_GeofenceName
+    total_geofences=geofences.count()
+    print("Total geofences: ",total_geofences)
 
-for i in range(0,total_geofences):
-    wrong_logs=df_book_land_pu[
-    ((df_book_land_pu['Events']=='Landmark OUT')&
-     (df_book_land_pu['Landmark']==geofences.iloc[i])|
-     (df_book_land_pu['Events']=='Landmark IN')&
-     (df_book_land_pu['Landmark']==geofences.iloc[i])
-    )
-    ]
+    print("Wrong logs: ")
+
+    for i in range(0,total_geofences):
+        wrong_logs=df_book_land_pu[
+        ((df_book_land_pu['Events']=='Landmark OUT')&
+        (df_book_land_pu['Landmark']==geofences.iloc[i])|
+        (df_book_land_pu['Events']=='Landmark IN')&
+        (df_book_land_pu['Landmark']==geofences.iloc[i])
+        )
+        ]
     if len(wrong_logs)!=0:
         print(wrong_logs)
 
