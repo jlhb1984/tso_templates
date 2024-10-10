f72470556b85bbdbe2e5b7c9b5788ee44b41ac49
jlhb1984 <jlhb1984@gmail.com>
2024-10-09 20:28:45 -0500

    Se agregó filtro de fecha usando dt.

--- date.py
+++ date.py
@@ -12,4 +12,7 @@ class Date():
         df_filter['Date']=pd.to_datetime(df['Date'])
         print(df_filter.info())        
         df.sort_values(by='Date', ascending=True)
-        print(df_filter.head(20))
+        print(df_filter)
+        df_filter_date=df_filter[(df_filter['Date'].dt.month>8)&(df_filter['Date'].dt.day>26)]
+        print(df_filter_date)
+        print("\n")
